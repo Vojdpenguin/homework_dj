2fa4cebb5d67ab4c669e50e4153782f3581c4d99
Vojd <regush20@gmail.com>
2024-12-18 19:49:43 +0200

    12

---
+++ .venv/notes/users/models.py
@@ -0,0 +1,21 @@
+from django.db import models
+from django.contrib.auth.models import User
+from PIL import Image
+# Create your models here.
+
+class Profile(models.Model):
+    user = models.OneToOneField(User, on_delete=models.CASCADE)
+    avatar = models.ImageField(default='default_avatar.png', upload_to='profile_images')
+
+    def __str__(self):
+        return self.user.username
+
+    def save(self, *args, **kwargs):
+        super().save()
+
+        img = Image.open(self.avatar.path)
+
+        if img.height > 250 or img.width > 250:
+            output_size = (250, 250)
+            img.thumbnail(output_size)
+            img.save(self.avatar.path)
